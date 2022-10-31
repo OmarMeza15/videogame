1410e488ece9e1fff5f4488aace6b33982a037fd
OmarMeza15 <omarmezajarquin@gmail.com>
2022-10-31 20:46:13 +0100

    added 2 enemies with their sprites and drawn them in the canvas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,44 +19,66 @@ g3.src = "/img/Characters/Cat/4.png";
 g4.src = "/img/Characters/Cat/5.png";
 g5.src = "/img/Characters/Cat/6.png";
 
+    // Enemy 1
+const e0 = new Image();
+const e1 = new Image();
+const e2 = new Image();
+const e3 = new Image();
+e0.src = "/img/Characters/Bat1/1.png";
+e1.src = "/img/Characters/Bat1/2.png";
+e2.src = "/img/Characters/Bat1/3.png";
+e3.src = "/img/Characters/Bat1/4.png";
+
+    // Enemy 2
+const e4 = new Image();
+const e5 = new Image();
+const e6 = new Image();
+const e7 = new Image();
+e4.src = "/img/Characters/Bat2/1.png";
+e5.src = "/img/Characters/Bat2/2.png";
+e6.src = "/img/Characters/Bat2/3.png";
+e7.src = "/img/Characters/Bat2/4.png";
+
     // Background
 const img = new Image();
 img.src = "/img/Backgrounds/loopbackground3.jpg";
 
 // Sprites
 const catSprites = [g0, g1, g2, g3, g4, g5];
+const bat1Sprites = [e0, e1, e2, e3, e2, e1];
+const bat2Sprites = [e4, e5, e6, e7, e6, e5];
 
 // Background loop
-const backgroundImage = {
-    img: img,
-    x: 0,
-    speed: -1,
+// const backgroundImage = {
+//     img: img,
+//     x: 0,
+//     speed: -1,
   
-    move: function() {
-      this.x += this.speed;
-      this.x %= img.width;
-    },
+//     move: function() {
+//       this.x += this.speed;
+//       this.x %= img.width;
+//     },
   
-    draw: function() {
-      ctx.drawImage(this.img, this.x, 0);
-      if (this.speed < 0) {
-        ctx.drawImage(this.img, this.x + img.width, 0);
-      } else {
-        ctx.drawImage(this.img, this.x - this.img.width, 0);
-      }
-    },
-  };
+//     draw: function() {
+//       ctx.drawImage(this.img, this.x, 0);
+//       if (this.speed < 0) {
+//         ctx.drawImage(this.img, this.x + img.width, 0);
+//       } else {
+//         ctx.drawImage(this.img, this.x - this.img.width, 0);
+//       }
+//     },
+//   };
   
-  function updateCanvas() {
-    backgroundImage.move();
+//   function updateCanvas() {
+//     backgroundImage.move();
   
-    ctx.clearRect(0, 0, 768, 468);
-    backgroundImage.draw();
+//     ctx.clearRect(0, 0, 768, 468);
+//     backgroundImage.draw();
   
-    requestAnimationFrame(updateCanvas);
-  }
+//     requestAnimationFrame(updateCanvas);
+//   }
 
-img.onload = updateCanvas();
+// img.onload = updateCanvas();
 
 // Define characters
 class cat {
@@ -101,29 +123,97 @@ class cat {
     }
 }
 
+class bat1 {
+    constructor(ctx, positionX, positionY, image) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.image = image;
+        this.ctx = ctx;
+    }
+
+    back() {
+        this.positionX = this.positionX - 5;
+    }
+
+    drawing() {
+        this.ctx.drawImage(this.image, this.positionX, this.positionY);
+    }
+}
+
+class bat2 {
+    constructor(ctx, positionX, positionY, image) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.image = image;
+        this.ctx = ctx;
+    }
+
+    back() {
+        this.positionX = this.positionX - 5;
+    }
+
+    drawing() {
+        this.ctx.drawImage(this.image, this.positionX, this.positionY);
+    }
+}
+
 // Character
 const character  = new cat(ctx, 40, 200, g0);
 
 // Counter
 let counter = 0;
 
-// 
+// Game start
 let intervalCharacter;
+let intervalEnemies;
+let intervalBat2;
 
 function startGame() {
+    character.life = 3;
+    enemies1 = [];
+    enemies2 = [];
     character.positionX = 40;
     character.positionY = 200;
 
     intervalCharacter = setInterval(() => {
         ctx.clearRect(0, 0, 768, 468);
         character.drawing();
+
+        enemies1.forEach((enemy1, enemyPosition) => {
+            enemy1.back();
+            enemy1.drawing();
+            enemy1.image = bat1Sprites[counter];
+        });
+
+        enemies2.forEach((enemy2, enemyPosition) => {
+            enemy2.back();
+            enemy2.drawing();
+            enemy2.image = bat2Sprites[counter];
+        });
     }, 1000 / 60);
+    
+    // Create enemies
+    intervalEnemies = setInterval(() => {
+        if(Math.floor(Math.random() * 2) == 1) {
+            const height1 = Math.floor(Math.random() * 200);
+
+            const enemy1 = new bat1(ctx, 768, height1, e0);
+            enemies1.push(enemy1);
+        }
+    }, 2000);
+
+    intervalBat2 = setInterval(() => {
+        if(Math.floor(Math.random() * 2) == 1) {
+            const height2 = Math.floor(Math.random() * 400);
+
+            const enemy2 = new bat2(ctx, 768, height2, e4);
+            enemies2.push(enemy2);
+        }
+    }, 2000);
 
     setInterval(() => {
         spritesAnimation();
-    }, 100);
-
-    
+    }, 200);
 }
 
 startGame();
@@ -147,7 +237,7 @@ window.addEventListener("keydown", (event) => {
 
 function spritesAnimation() {
     character.image = catSprites[counter];
-    if (counter < 5) {
+    if(counter < 5) {
         counter++;
       } else {
         counter = 0;
